66face30985dc156998ad6fe29293816244a499a
ramseypapii <ramsey.kuwona@student.moringaschool.com>
2023-12-13 08:33:45 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fib_sequence = [0, 1]
+    for _ in range(2, length):
+        next_number = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_number)
+    print(fib_sequence[:length])
